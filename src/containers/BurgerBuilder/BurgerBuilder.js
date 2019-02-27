78f7bf154a105f4986f3b963a5fa5d956396e8d6
Micah Lund <micah.r.lund@gmail.com>
2019-02-27 11:02:44 -0700

    added build controls and more styling to our burger builder. Troubleshooting an issue with the reduce() method, but wanted to commit my current progress in the meantime

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
   //constructor(props){
@@ -10,18 +11,18 @@ class BurgerBuilder extends Component {
 
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   };
 
   render() {
     return (
       <Aux>
-        <Burger ingredients={this.state.ingredients}/>
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </Aux>
     );
   }
